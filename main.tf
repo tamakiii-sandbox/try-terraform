1e9746e9d7e9fe0eb115426516d0f5cdaece820a
tamakiii <tamaki.htm@gmail.com>
2017-12-21 10:17:09 +0900

    add s3 backend

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 
 terraform {
   require_versions = ">= 0.11.1"
+
+  backend "s3" {
+    region = "ap-northeast-1"
+    bucket = "terraform.tamakiii.com"
+    key = "terraform.tfstate"
+    profile = "tamakiii"
+  }
 }
